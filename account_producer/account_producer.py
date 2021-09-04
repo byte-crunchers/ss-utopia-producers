98e505b1b23862acf9e22b78bc70a0f7f293bb0b
henry-arjet-ss <henry.arjet@smoothstack.com>
2021-09-03 20:36:36 -0500

    finished functionality for account_producer.py

--- account_producer/account_producer.py
+++ account_producer/account_producer.py
@@ -1,5 +1,69 @@
+import random
+import traceback
 import mysql
 from mysql.connector import Error
+import datetime
+
+
+
+class Account:
+    def __init__(self):
+            self.user = None
+            self.account_type = None
+            self.balance = None
+            self.payment_due = None
+            self.due_date = None
+            self.limit = None
+
+def get_users(conn):
+    cur = conn.cursor()
+    query = "SELECT id FROM users"
+    cur.execute(query)
+    return cur.fetchall()
+
+def get_account_types(conn):
+    cur = conn.cursor()
+    query = "SELECT id FROM account_type"
+    cur.execute(query)
+    return cur.fetchall()
+
+
+def create_account(user, account_type): #takes user account number, returns account object
+    account = Account()
+    account.user = user
+    account.account_type = account_type
+    if (account_type == "Checking" or account_type == "Savings"): #non-loan accounts
+        account.balance = random.uniform(0, 4206.9)
+        account.payment_due = 0
+    else:
+        account.limit = random.uniform(-4206.9, 0)
+        account.balance = random.uniform(account.limit, 0)
+        account.payment_due = account.balance * -0.10 #pay 10%
+        account.due_date = datetime.date.today() + datetime.timedelta(days = random.randrange(1,31))
+    return account
+    
+def generate(num_rows, conn):
+    users_all = get_users(conn)
+    if (len(users_all) < num_rows//2+1):
+        print ("Not enough users in the database to support that many rows \n"\
+            +"The database only supports {:d} rows".format(len(users_all) * 2 - 1))
+        return 1
+    users = random.sample(users_all, num_rows//2+1) #gets a random sampling of users
+                                                #//2 means the average user will have two accounts 
+    query = "INSERT INTO accounts VALUES (0,%s,%s,%s,%s,%s,%s)"
+    acc_types = get_account_types(conn)
+    cur = conn.cursor()
+    for i in range (num_rows):
+        account = create_account(random.choice(users)[0], random.choice(acc_types)[0]) #takes a random user id and account type
+        vals = (account.user, account.account_type, account.balance, account.payment_due,\
+         account.due_date, account.limit)
+        try:
+            cur.execute(query, vals)
+        except Error:
+            print("There was a problem writing to the database. ")
+            traceback.print_exc()
+
+    conn.commit()
 
 def connect():
     con_try = None
@@ -14,3 +78,16 @@ def connect():
     else:
         print("There was a problem connecting to the database, please make sure the database information is correct!")
         print(Error)
+
+
+def clear(conn):
+    cur = conn.cursor()
+    query = "DELETE FROM accounts"
+    cur.execute(query)
+    #doesn't commit until the generate function
+    
+
+if __name__ == '__main__':
+    conn = connect()
+    clear(conn)
+    generate(100, conn)
