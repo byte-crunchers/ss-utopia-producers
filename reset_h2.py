60d9912d4c197bae298749a2a6b11ca78830e03c
henry-arjet-ss <henry.arjet@smoothstack.com>
2021-09-11 18:31:06 -0500

    bc-134 made loan payment producer

--- reset_h2.py
+++ reset_h2.py
@@ -3,7 +3,7 @@ import jaydebeapi
 
 def reset_db():
     con = jaydebeapi.connect("org.h2.Driver", "jdbc:h2:tcp://localhost/~/test;MODE=MySQL", ["sa", ""], "E:/Program Files (x86)/H2/bin/h2-1.4.200.jar" )
-    f = open("db-dump.sql")
+    f = open("h2-dump.sql")
     con.cursor().execute(f.read())
     con.commit()
     con.close()
