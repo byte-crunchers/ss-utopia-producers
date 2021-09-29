0e3c887e627f8e9792a54bdca1841edffdbdc1aa
WyattMeehanSmoothStack <wyatt.meehan@smoothstack.com>
2021-09-29 10:17:16 -0400

    BC-169 - Tech Debt - Normalizing location on Branch Producer

--- branch_producer/branch_producer.py
+++ branch_producer/branch_producer.py
@@ -12,42 +12,56 @@ zip_database = ZipCodeDatabase()
 valid_zips = []
 for x in zip_database:
     valid_zips.append(x)
+choice = random.choice(valid_zips)
 
 
 class Branch:
-    def __init__(self, location):
-        self.location = location
+    def __init__(self, street_address, city, state, zip):
+        self.street_address = street_address
+        self.city = city
+        self.state = state
+        self.zip = zip
+    def print_branch(self):
+        print(self.street_address, self.city, self.state, self.zip)
 
 
-def get_address():
-    choice = random.choice(valid_zips)
+def get_street_address():
+    return fake.building_number() + " " + fake.street_name() + "\n"
+
+
+def get_city():
+    return fake.city()
+
+
+def get_zip_and_state():
     zip_code = zip_database[choice]
     state = zip_code.state
-    return fake.building_number() + " " + fake.street_name() + ",\n" + fake.city() + ", " + state + " " + choice
+    return zip_code.zip, state
 
 
 def generate_branches(num_of_branches):
     branches = []
-    for _ in range(num_of_branches):
-        location = get_address()
-        branches.append(Branch(location))
+    for _ in range(0, num_of_branches):
+        state = get_zip_and_state()[1]
+        zip = get_zip_and_state()[0]
+        branches.append(Branch(get_street_address(), get_city(), state, zip))
     return branches
 
 
-def populate_branches(branch_data, pop_conn, pop_table):
+def populate_branches(branch_data, pop_conn):
     duplicate_count = 0
     dd_count = 0
     curs = pop_conn.cursor()
+    query = "INSERT INTO branches (street_address, city, state, zip) VALUES (?, ?, ?, ?)"
     for branch in branch_data:
-        query = "INSERT INTO {}(location) VALUES ('{}')".format(pop_table, branch.location)
         try:
+            vals = (branch.street_address, branch.city, branch.state, branch.zip)
             curs.execute(query)
         except (mysql.connector.errors.IntegrityError, jaydebeapi.DatabaseError):  # Check for Duplicates
             duplicate_count += 1
             while True:
                 try:
-                    query = "INSERT INTO {}(location) VALUES ('{}')".format(pop_table, get_address())
-                    curs.execute(query)
+                    curs.execute(query, vals)
                     break
                 except (mysql.connector.errors.IntegrityError, jaydebeapi.DatabaseError):
                     dd_count += 1
@@ -60,4 +74,6 @@ def populate_branches(branch_data, pop_conn, pop_table):
 
 
 if __name__ == '__main__':
-    get_address()
+    branches = generate_branches(10)
+    for b in branches:
+        b.print_branch()
