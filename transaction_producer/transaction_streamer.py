a1cf59e76aaa564899083cdd63f7f3a4e56bba1a
WyattMeehanSmoothStack <wyatt.meehan@smoothstack.com>
2021-10-20 16:19:48 -0400

    Streamers Hotfix - changed transaction streamers to using environment variables and region configuration

--- transaction_producer/transaction_streamer.py
+++ transaction_producer/transaction_streamer.py
@@ -1,33 +1,43 @@
-import transaction_producer as tp
-import time
 import json
-import boto3
+import os
 import random
+import time
+
+import boto3
+from botocore.config import Config
 from faker import Faker
 
-def loopers(accounts: tuple, fake: Faker, kin ) -> None:
-    accounts_sample = random.sample(accounts, 2) #use so that we can have two random, unique accounts
+import transaction_producer as tp
+
+access_key = os.environ.get("ACCESS_KEY")
+secret_key = os.environ.get("SECRET_KEY")
+
+my_config = Config(
+    region_name='us-east-1'
+)
+
+
+def loopers(accounts: tuple, fake: Faker, kin) -> None:
+    accounts_sample = random.sample(accounts, 2)  # use so that we can have two random, unique accounts
     trans = tp.Transaction(fake, accounts_sample[0][0], accounts_sample[1][0])
-    trans 
-    trans.type = 'transaction' #for the consumer to know what type of message it is
+    trans
+    trans.type = 'transaction'  # for the consumer to know what type of message it is
     print(json.dumps(trans.__dict__, default=str))
     kin.put_record(StreamName='byte-henry', Data=json.dumps(trans.__dict__, default=str), PartitionKey='trans key')
-    
+
 
 def stream(interval: float = 5, chance: float = 1) -> None:
     accounts = tp.get_accounts(tp.connect())
-    if (len(accounts) == 0):
+    if len(accounts) == 0:
         print("ERROR: missing accounts from database")
         return 1
-    f = open('awskey.json', 'r')
-    key = json.load(f)      
-    kin = boto3.client('kinesis', aws_access_key_id=key['access_key'], aws_secret_access_key=key['secret_key'])
+    kin = boto3.client('kinesis', config=my_config, aws_access_key_id=access_key, aws_secret_access_key=secret_key)
     fake = Faker()
-    while(True):
-        if random.random() < chance: #are we going to add a record this interval
+    while True:
+        if random.random() < chance:  # are we going to add a record this interval
             loopers(accounts, fake, kin)
         time.sleep(interval)
 
+
 if __name__ == "__main__":
     stream(0.4, 0.2)
-    
