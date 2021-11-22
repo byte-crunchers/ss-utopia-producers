c499ce842f516ad24a2e80be0362c5bb2b6b089e
WyattMeehanSmoothStack <wyatt.meehan@smoothstack.com>
2021-11-22 13:27:08 -0500

    DEVELOP - Acoount Producer Hotfix - Made account producer work with new schema, added mysql driver

--- account_producer/account_producer.py
+++ account_producer/account_producer.py
@@ -32,11 +32,12 @@ class Account:
         self.balance = None
         self.payment_due = None
         self.due_date = None
-        self.limit = None
+        self.credit_limit = None
         self.interest = None
         self.active = False
         self.approved = False
         self.confirmed = False
+        self.limit = None
 
 
 def get_users(conn):
@@ -57,13 +58,14 @@ def create_account(user, account_type):  # takes user account number, returns ac
     account = Account()
     account.user = user
     account.account_type = account_type
+    account.limit = 10000
     if account_type == "Checking" or account_type == "Savings":  # non-loan accounts
         account.balance = random.uniform(0, 10000)
         account.payment_due = 0
         account.interest = 0
     else:
-        account.limit = random.uniform(-4206.9, 0)
-        account.balance = random.uniform(account.limit, 0)
+        account.credit_limit = random.uniform(-4206.9, 0)
+        account.balance = random.uniform(account.credit_limit, 0)
         account.payment_due = account.balance * -0.10  # pay 10%
         account.due_date = datetime.date.today() + datetime.timedelta(days=random.randrange(1, 31))
         account.interest = random.random() * 0.1 + 0.03  # random interest from 3% to 13%
@@ -86,16 +88,17 @@ def generate(num_rows, conn):
         return 1
     users = random.sample(users_all, num_rows // 2 + 1)  # gets a random sampling of users
     # //2 means the average user will have two accounts
-    query = 'INSERT INTO accounts(users_id, account_type, balance, payment_due, due_date, credit_limit, ' \
-            'debt_interest, active, approved, confirmed) VALUES (?,?,?,?,?,?,?,?,?,?) '
+    query = "INSERT INTO `bytecrunchers`.`accounts` (`users_id`, `account_type`, `balance`, `payment_due`," \
+            " `due_date`, `credit_limit`, `debt_interest`, `active`, `approved`, `confirmed`, `limit`)" \
+            " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"
     acc_types = get_account_types(conn)
     cur = conn.cursor()
     for i in range(num_rows):
         account = create_account(random.choice(users)[0],
                                  random.choice(acc_types)[0])  # takes a random user id and account type
         vals = (account.user, account.account_type, account.balance, account.payment_due,
-                date_to_string(account.due_date), account.limit, account.interest, account.active, account.approved,
-                account.confirmed)
+                date_to_string(account.due_date), account.credit_limit, account.interest, account.active, account.approved,
+                account.confirmed, account.limit)
         try:
             cur.execute(query, vals)
         except Error:
